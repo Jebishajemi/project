2aba36666d8d884c6db4d487dfd017fcb6bb220b
Jebisha <129985841+Jebishajemi@users.noreply.github.com>
2026-02-23 09:57:13 +0530

    Add Candidate class with properties and constructor

---
+++ Candidate.java
@@ -0,0 +1,12 @@
+public class Candidate {
+
+    int id;
+    String name;
+    int votes;
+
+    public Candidate(int id, String name, int votes) {
+        this.id = id;
+        this.name = name;
+        this.votes = votes;
+    }
+}
