3bab557e82318c811ea221bea9c1cdc341b440b2
Jebisha <129985841+Jebishajemi@users.noreply.github.com>
2026-02-23 10:00:02 +0530

    Add Voter class with properties and constructor

---
+++ Voter.java
@@ -0,0 +1,12 @@
+public class Voter {
+
+    int id;
+    String name;
+    boolean hasVoted;
+
+    public Voter(int id, String name, boolean hasVoted) {
+        this.id = id;
+        this.name = name;
+        this.hasVoted = hasVoted;
+    }
+}
